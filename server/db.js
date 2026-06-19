0ff2b9168a71f2a81d656d142dd9b14f7a1ea19e
ash.nrb <nirobash1@gmail.com>
2026-06-20 05:20:37 +1000

    Add SSL and port config for cloud database

--- server/db.js
+++ server/db.js
@@ -6,7 +6,8 @@ const pool = mysql.createPool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  port: process.env.DB_PORT || 3306,
+  ssl: process.env.DB_SSL === 'true' ? { minVersion: 'TLSv1.2' } : undefined,
 });
 
-// Export the promise-based version so we can use async/await
 module.exports = pool.promise();
